43150deaa06079289bc48194fddd8f428da595d0
axido <axido@users.noreply.github.com>
2015-07-01 14:11:51 +0300

    Cleaning up stuff
    Replaced .replace spam with a simple regex. Also made messages bit nicer.

--- Palindrome.py
+++ Palindrome.py
@@ -1,20 +1,21 @@
 # Just a small programming exercise. Checks if the given word or sentence is a palindrome.
+import re
 
 print("Tämä ohjelma kertoo, onko annettu sana tai lause palindromi.")
-print("Voit poistua ohjelmasta komennolla \"quit\".")
+print("Voit poistua ohjelmasta komennolla \"exit\".")
 
 while True:
     # Kysytään käyttäjältä sanaa tai lausetta, sitten
     # Poistetaan yleisimmät välimerkit, muutetaan pienaakkosiksi vertailun helpottamiseksi.
     i = input("Anna palaa: ")
-    k = i.lower().replace(" ", "").replace(",", "").replace("!", "").replace(":", "").replace(";", "").replace(".", "")
-    if k == "quit":
+    k = re.sub(r'[,!:;.]|\s', '', i).lower()
+    if k == "exit":
         break
     if any(char.isdigit() for char in k) == True:  # Tarkistetaan onko mukana numeroita tai onko arvo tyhjä.
         print("Numerot eivät ole sallittuja.")
-    elif not k:
+    elif not k or k == '':
         print("Et kirjoittanut mitään!")
     elif k[::-1] == k:  # Tarkistaa, onko käännetty arvo ja alkuperänen samat. Niiden täsmätessä lause on palindromi.
-        print("Antamasi sana tai lause on ehta palindromi!")
+        print("", i, " on palindromi!", sep='"')
     else:
         print("Valitettavasti ", i, " ei ole palindromi.", sep='"')
