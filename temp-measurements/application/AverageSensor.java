9b62bcdb9f7d182256b4008f7aac0fb3b57299a0
axido <axido@users.noreply.github.com>
2016-03-18 21:47:55 +0200

    Update and rename src/application/AverageSensor.java to temp-measurements/application/AverageSensor.java

--- temp-measurements/application/AverageSensor.java
+++ temp-measurements/application/AverageSensor.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * An average sensor contains many sensors. Measure method returns the average of the readings of all its sensors.
  */
 package application;
 
